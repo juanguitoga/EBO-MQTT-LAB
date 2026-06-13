47c49ba3ca1c879e854e301d9a6df0493d934368
juanguitoga <139376726+juanguitoga@users.noreply.github.com>
2026-06-13 13:21:33 -0500

    Update send_mqtt.py

--- send_mqtt.py
+++ send_mqtt.py
@@ -5,18 +5,28 @@ import paho.mqtt.client as mqtt
 BROKER = "687911b1a56e45cb86a3e073a1f868ca.s1.eu.hivemq.cloud"
 PORT = 8883
 USERNAME = "EBOEXAMPLE"
-PASSWORD = os.environ["MQTT_PASSWORD"]
+PASSWORD = os.environ.get("MQTT_PASSWORD")
 
-client = mqtt.Client()
+print("Password encontrada:", PASSWORD is not None)
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
 client.username_pw_set(USERNAME, PASSWORD)
 
 client.tls_set(cert_reqs=ssl.CERT_REQUIRED)
 
-client.connect(BROKER, PORT)
+print("Intentando conectar...")
 
-client.publish("weather/test", "50")
+client.connect(BROKER, PORT, 60)
 
-print("Mensaje enviado: 50")
+print("Conectado")
+
+info = client.publish("weather/test", "50")
+
+print("Código publicación:", info.rc)
+
+client.loop(timeout=2)
 
 client.disconnect()
+
+print("Terminado")
